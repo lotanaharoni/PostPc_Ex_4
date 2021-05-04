c15380414374fa2723f995a3ead1a6bec650a7cb
lotanaharoni <lotanaharoni@gmail.com>
2021-05-05 00:35:09 +0300

    Successed to transfer intent

--- app/src/main/java/exercise/find/roots/RootsResults.java
+++ app/src/main/java/exercise/find/roots/RootsResults.java
@@ -1,7 +1,10 @@
 package exercise.find.roots;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,13 @@ public class RootsResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_roots_results);
+
+        TextView firstRoot = findViewById(R.id.FirstRoot);
+
+        Intent intent = getIntent();
+
+        firstRoot.setText(String.valueOf(intent.getLongExtra("root1", 0)));
+
     }
 
 }
